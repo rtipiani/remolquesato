4f559406544f70b1b27746871ff79a15b1aaa046
Ruben Tipiani <rtipiani@gmail.com>
2024-12-03 20:21:49 -0500

    Primer commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#15803d',
+				secondary: '#70d959'
+			}
+		},
+	},
+	plugins: [],
+}
