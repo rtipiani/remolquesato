bdd24e8d7e843beacb747941814a04ebe34ef51c
Ruben Tipiani <rtipiani@gmail.com>
2024-12-09 11:56:35 -0500

    Final

--- convert-images.mjs
+++ convert-images.mjs
@@ -2,29 +2,26 @@ import fs from 'fs';
 import path from 'path';
 import sharp from 'sharp';
 
-const inputDir = path.join(process.cwd(), 'public/images'); // Directorio de entrada
-const outputDir = inputDir; // Usamos el mismo directorio para sobrescribir las imágenes originales
+const inputDir = path.join(process.cwd(), 'public/images');
+const outputDir = inputDir;
 
-// Función para convertir imágenes a WebP
 const convertImagesToWebP = (inputDir, outputDir) => {
-  const files = fs.readdirSync(inputDir); // Leemos todos los archivos en el directorio
+  const files = fs.readdirSync(inputDir);
 
   files.forEach((file) => {
-    const extname = path.extname(file).toLowerCase(); // Obtenemos la extensión del archivo
-    if (extname === '.jpg' || extname === '.png') {   // Si el archivo es JPG o PNG
-      const inputPath = path.join(inputDir, file);   // Ruta completa del archivo de entrada
-      const outputFileName = path.basename(file, extname) + '.webp'; // Nombre del archivo de salida en formato WebP
-      const outputPath = path.join(outputDir, outputFileName); // Ruta completa del archivo de salida
+    const extname = path.extname(file).toLowerCase();
+    if (extname === '.jpg' || extname === '.png') {
+      const inputPath = path.join(inputDir, file);
+      const outputFileName = path.basename(file, extname) + '.webp';
+      const outputPath = path.join(outputDir, outputFileName);
 
-      // Usamos sharp para convertir la imagen
       sharp(inputPath)
-        .webp({ quality: 80 }) // Convertimos a WebP con una calidad de 80 (ajustable)
+        .webp({ quality: 80 })
         .toFile(outputPath, (err, info) => {
           if (err) {
             console.error('Error al convertir la imagen:', err);
           } else {
             console.log(`Imagen convertida a WebP: ${outputPath}`);
-            // Elimina la imagen original (si deseas reemplazarla)
             fs.unlinkSync(inputPath);
           }
         });
@@ -32,5 +29,4 @@ const convertImagesToWebP = (inputDir, outputDir) => {
   });
 };
 
-// Ejecutamos la conversión
 convertImagesToWebP(inputDir, outputDir);
