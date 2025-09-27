089d1770dfec9cb847fe43005afd8bcdcc8e18c2
Rubén Tipiani <38152657+rtipiani@users.noreply.github.com>
2025-09-27 05:03:06 -0500

    Versión optimizada

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,ts}'],
 	theme: {
 		extend: {
 			colors: {
@@ -10,4 +10,5 @@ export default {
 		},
 	},
 	plugins: [],
+	mode: 'jit',
 }
