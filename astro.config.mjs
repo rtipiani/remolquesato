6b342118a52ebfa9003d263551b00bfba16f87d0
Rubén Tipiani <38152657+rtipiani@users.noreply.github.com>
2026-09-01 17:11:18 -0500

    chore: optimizacion de SEO y rendimiento (PageSpeed)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://remolquesato.com',
+  integrations: [tailwind(), sitemap()]
 });
